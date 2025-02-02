0b9b579ffdee5ea49b53e5fb5b7866a435f0f01e
MuhammadShahroz <159851369+Mueza-Ejaz@users.noreply.github.com>
2025-02-03 03:26:59 +0500

    Update statelibrary.ts

--- src/statelibrary.ts
+++ src/statelibrary.ts
@@ -1,27 +1,25 @@
-import { atom } from 'jotai'
-import { atomWithStorage } from 'jotai/utils'
+
+import { atomWithStorage } from "jotai/utils";
 
 
 
-interface Products {
-    name: string;
-    quantity: number;
-    id: string;
-    image: string;
-    description: string;
-    price: number;
-    category: string;
-    status: string;
-    inventory: number;
-    colors: string[];
-    productName: string;
-    _id: string;
-  }
-  
-  interface Params {
-    id: string;
-  }
+// types.ts
+export interface Product {
+  _id: string;
+  image: string;
+  description: string;
+  price: number;
+  category: string;
+  status: string;
+  inventory: number;
+  colors: string[];
+  productName: string;
+  // Add any other fields from your Sanity schema
+}
 
+export interface CartItem extends Product {
+  quantity: number;
+  selectedColor: string;
+}
 
-
-export const cartsItems = atomWithStorage<Products []>('cartsItems', [])
+export const cartsItems = atomWithStorage<CartItem[]>("cartItems", []);
