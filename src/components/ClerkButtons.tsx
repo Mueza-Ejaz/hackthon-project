3826db4a0a4ed4014c277cf1cbac39d302dd8514
MuhammadShahroz <159851369+Mueza-Ejaz@users.noreply.github.com>
2025-02-03 14:56:30 +0500

    Update ClerkButtons.tsx

--- src/components/ClerkButtons.tsx
+++ src/components/ClerkButtons.tsx
@@ -1,17 +1,26 @@
-"use client";
 
-import { useUser } from "@clerk/nextjs";
-import Link from "next/link";
+
+import { useUser , SignInButton } from "@clerk/nextjs";
+import { motion } from "framer-motion";
 
 const ClerkButtons = () => {
   const { isSignedIn } = useUser();
 
   return isSignedIn ? null : (
-    <Link href="/sign-in">
-      <button className="bg-blue-500 py-2 px-4 text-white rounded-md hover:bg-blue-700">
-        Sign In
-      </button>
-    </Link>
+    
+
+    
+        <SignInButton>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="bg-blue-500 py-[2px] px-3 text-[12px] text-white rounded-md hover:bg-blue-700"
+          >
+            Sign In
+          </motion.button>
+        </SignInButton>
+
+  
   );
 };
 
