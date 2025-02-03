58492b7c97010cc0b7b9e1c287d727f347c4d854
MuhammadShahroz <159851369+Mueza-Ejaz@users.noreply.github.com>
2025-02-03 14:56:33 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-const isPublicRoute = createRouteMatcher(['/','/allproduct/:path*', '/carts']);
+
+const isPublicRoute = createRouteMatcher([
+  '/',              // Home route
+  '/public(.*)',     // All files inside public folder
+  '/allproduct/:path*',
+  '/carts'
+]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect()
+    await auth.protect();  // Protecting all routes except public ones
   }
-})
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Exclude internal Next.js files and public folder files from the middleware
+    '/((?!_next|public|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+};
