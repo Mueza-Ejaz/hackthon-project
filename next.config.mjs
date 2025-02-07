59c7c436538e2572fbea8534296b41ce4ef701cd
Mueza Ejaz <159851369+Mueza-Ejaz@users.noreply.github.com>
2025-02-07 15:21:06 +0500

    *Using* Internationalization (i18n) in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['cdn.sanity.io'], // Add Sanity's CDN hostname here
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['cdn.sanity.io'], // Add Sanity's CDN hostname
+  },
+
+  i18n: {
+    locales: ["en", "fr", "es"], // Supported languages
+    defaultLocale: "en", // Default language
+  },
+};
+
+export default nextConfig;
